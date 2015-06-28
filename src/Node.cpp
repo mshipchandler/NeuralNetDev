c4b234d1910329512ea366400af4f7cb340c4305
mshipchandler <maadms@gmail.com>
2015-06-28 17:34:42 -0400

    Modularize bias role in delta calculation

--- src/Node.cpp
+++ src/Node.cpp
@@ -113,11 +113,11 @@ void Node::updateWeights()
 	double deltaWeight = 0.0, updatedWeight = 0.0;
 	for(int i = 0; i < (int)weight_port.size(); i++)
 	{
-		/*// If it is a bias node, node value doesn't come into play, becuase the node value is always 1, generally.
-		if(weight_port[i]->isBias()) 
-			deltaWeight = ALPHA * errorGradient; // This can be omitted, since the following equation is sufficient. -MS
-		else*/
-			deltaWeight = ALPHA * weight_port[i]->getNodeVal() * errorGradient; // nodeVal of bias is 1, so above part can be omitted.
+		// If it is a bias node, node value doesn't come into play, becuase the node value is always 1, generally.
+		if(weight_port[i]->isBias())
+			deltaWeight = ALPHA * errorGradient;
+		else
+			deltaWeight = ALPHA * weight_port[i]->getNodeVal() * errorGradient;
 
 		updatedWeight = weight_port[i]->getWeight(this) + deltaWeight;
 		weight_port[i]->setWeight_forUpdate(updatedWeight, this);
